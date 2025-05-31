606253614fe782558f7eb721ed05b8c351aa851d
Sarthak Jain <93116216+SarthakJain29@users.noreply.github.com>
2025-05-31 12:50:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const buttons = document.querySelecterAll("button");
+
+buttons.forEach((btn)=>{
+	btn.addEventListener("click", ()=>{
+		const soundName = btn.textContent.trim();
+		const audio = new Audio(`sounds/${soundName}.mp3`);
+		audio.play();
+	});
+});
